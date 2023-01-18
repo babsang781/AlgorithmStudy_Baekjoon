018ee918e78e93560684800c11a4dc97b5f993c2
박상민 <89438832+babsang781@users.noreply.github.com>
2023-01-19 02:44:09 +0900

    AMC hotel

--- AlgorithmStudy_Baekjoon/AMC_Hotel.cs
+++ AlgorithmStudy_Baekjoon/AMC_Hotel.cs
@@ -6,17 +6,60 @@ using System.Threading.Tasks;
 
 namespace AlgorithmStudy_Baekjoon
 {
-    internal class AMC_Hotel
+ /*   class Program
     {
-        public int[] Solution(int[] q)
+        static void Main(string[] args)
         {
-            int[] result = new int[q.Length -1];
+            StringBuilder stringBuilder = new StringBuilder();
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; ++i)
+            {
+                string[] q = Console.ReadLine().Split();
+                stringBuilder.AppendLine(((q[i + 1][2] % q[i + 1][0]) * 100 + (q[i + 1][2] / q[i + 1][0]) + 1).ToString());
+            }
+            Console.WriteLine(stringBuilder);
 
 
+        }
+    }*/
 
+    /*
+    // 1차 시도- 제출 방식 틀림 - 컴파일 에러.. 역시 프로그래머스가 친절한데 ㅜㅜ
+    // 백준은 시작이 너무 어렵다.
 
-            return result;
+    public int[] Solution(int[][] q)
+    {
+        int[] result = new int[q.Length-1];
+        // q[1][0] = 층 수, q[1][1] = 방 수, q[1][2] = 손님 수, (문제에서 몫은 98을 넘지 않고, 손님 수는 문제의 범위를 벗어나지 않는다도 함.)
+        // 문제를 읽고 산수 값의 몫과 나머지를 어떻게 구성할지 생각하여 배열에 적절히 집어넣는 문제
+        //-> 나머지에 따라 층이 정해지고,호수는 (몫+1)의 값임. 
+        for (int i = 0;i< q.Length - 1; i++)
+        {
+            result[i] = (q[i + 1][2] % q[i + 1][0]) * 100 + (q[i + 1][2] / q[i + 1][0]) + 1;
         }
 
+        return result;
     }
+
+    AMC_Hotel amcHotel = new AMC_Hotel();
+
+    Console.WriteLine("Hello, World!");
+
+    int[][] q = new int[3][]
+    {
+        new int[] {2},
+        new int[] {6, 12, 10 },
+        new int[] {30, 50, 72}
+    };
+
+    //Console.WriteLine(amcHotel.Solution(q));
+
+    for (int i = 0; i < amcHotel.Solution(q).Length; i++)
+    {
+        Console.WriteLine(amcHotel.Solution(q)[i]);
+    }
+
+*/
+
 }
--- AlgorithmStudy_Baekjoon/Program.cs
+++ AlgorithmStudy_Baekjoon/Program.cs
@@ -1,3 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using AlgorithmStudy_Baekjoon;
+
+
+using System;
+using System.Text;
+
+namespace backjoon
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; ++i)
+            {
+                string[] q = Console.ReadLine().Split();
+                int h = int.Parse(q[0]);
+                int w = int.Parse(q[1]);
+                int n = int.Parse(q[2]);
+                stringBuilder.AppendLine(
+                    ((n % h == 0 ? h : n % h) * 100 + 
+                    (n%h == 0 ? n/h : n/h+1)
+                    ).ToString());
+            }
+            Console.WriteLine(stringBuilder);
+
+
+        }
+        
+    }
+}
+
+
 
