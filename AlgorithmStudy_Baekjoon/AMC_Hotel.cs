443ec9c5d6cae0aed58b84c48287715c0199ce4d
박상민 <89438832+babsang781@users.noreply.github.com>
2023-01-19 00:51:38 +0900

    Second commit ...

--- AlgorithmStudy_Baekjoon/AMC_Hotel.cs
+++ AlgorithmStudy_Baekjoon/AMC_Hotel.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace AlgorithmStudy_Baekjoon
 {
-    internal class Class1
+    internal class AMC_Hotel
     {
+        public int[] Solution(int[] q)
+        {
+            int[] result = new int[q.Length -1];
+
+
+
+
+            return result;
+        }
+
     }
 }
