e302b5421e42720d598a68a94f1375f63729a201
박상민 <89438832+babsang781@users.noreply.github.com>
2023-01-24 23:52:01 +0900

    [Bronze III] Title: ACM 호텔, Time: 88 ms, Memory: 5856 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Text;
+
+namespace backjoon
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; ++i)
+            {
+                string[] q = Console.ReadLine().Split();
+                int h = int.Parse(q[0]);
+                int w = int.Parse(q[1]);
+                int n = int.Parse(q[2]);
+                stringBuilder.AppendLine(
+                    ((n % h == 0 ? h : n % h) * 100 + 
+                    (n%h == 0 ? n/h : n/h+1)
+                    ).ToString());
+            }
+            Console.WriteLine(stringBuilder);
+
+
+        }
+        
+    }
+}
