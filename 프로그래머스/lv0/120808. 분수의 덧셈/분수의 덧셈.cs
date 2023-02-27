4b777b5f31ba90b994b32b82d68437ad48fe68bb
박상민 <89438832+babsang781@users.noreply.github.com>
2023-02-28 01:26:02 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.19 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        
+        int num1 = numer1 * denom2 + numer2 * denom1;
+        int num2 = denom1 * denom2;
+        
+        for ( int i = num2 ; i >= 2;i--){
+            if( num1%i == 0 && num2%i ==0){
+                num1/=i;
+                num2/=i;
+            }
+        }
+        
+        int[] answer = new int[] {num1, num2 };
+        return answer;
+    }
+}
