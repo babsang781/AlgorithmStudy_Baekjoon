cb8fded91082d86354514091df463f308ad6443b
박상민 <89438832+babsang781@users.noreply.github.com>
2023-02-28 21:03:32 +0900

    [level 0] Title: 각도기, Time: 0.20 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.cs
+++ 프로그래머스/lv0/120829. 각도기/각도기.cs
@@ -2,16 +2,27 @@ using System;
 
 public class Solution {
     public int solution(int angle) {
-        int result = 0;
         
-        if( angle < 90 ) result = 1;
-        else if( angle == 90 ) result = 2;
-        else if( angle < 180 ) result = 3;
+        if( angle < 90 ) return 1;
+        else if( angle == 90 ) return  2;
+        else if( angle < 180 ) return 3;
         else
         {
-            result = 4;
+            return 4;
         }
         
-        return result;
+        
+        
     }
 }
+
+//int result = 0;
+//         if( angle < 90 ) result = 1;
+//         else if( angle == 90 ) result = 2;
+//         else if( angle < 180 ) result = 3;
+//         else
+//         {
+//             result = 4;
+//         }
+        
+//         return result;
