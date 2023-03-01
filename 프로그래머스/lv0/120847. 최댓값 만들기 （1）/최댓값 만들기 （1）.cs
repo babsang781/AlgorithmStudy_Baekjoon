fdd38a36ea96c915039e05351dd0082c6438fcb9
박상민 <89438832+babsang781@users.noreply.github.com>
2023-03-01 13:07:37 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 3.45 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cs
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cs
@@ -3,31 +3,25 @@ using System.Linq;
 
 public class Solution {
     public int solution(int[] numbers) {
-        int max = 0;
-        int max2 = 0;
         
-        for ( int i = 0 ; i < numbers.Length ; i++){
-            if( numbers[i] >= max2){
-                if( numbers[i] >= max){
-                    max2 = max;
-                    max = numbers[i];    
-                }else{
-                    max2 = numbers[i];
-                }
-            }
-            
-        }
+//         Array.Sort(numbers);
+//         Array.Reverse(numbers);
         
-        return max * max2;
+        numbers = numbers.OrderByDescending(x => x).ToArray();
+        
+        return numbers[0] * numbers[1];
     }
 }
 
-// 런타임 에러 2개  
+
+
+
+// 1. 런타임 에러 2개  
 // int max1 = numbers.Max();
 // int max2 = numbers.Where(x => x != max1).Max();
 // return max1 * max2;
 
-// 인덱스 저장해서 풀기, 첫 인덱스 처리 방법이 어렵네
+// 2. 인덱스 저장해서 풀기, 첫 인덱스 처리 방법이 어렵네
 // int[] arr = new int [2]{ 0, 0};
 // bool check = false;
 // for ( int i = 0 ; i < numbers.Length ; i++ ){
@@ -43,3 +37,17 @@ public class Solution {
 // }
 // }
 // return check ? numbers[arr[1]] * numbers[arr[0]] : 0;
+
+// 3. 반복문 방식- 통과 
+// int max = 0;
+// int max2 = 0;
+// for ( int i = 0 ; i < numbers.Length ; i++){
+//     if( numbers[i] >= max2){
+//         if( numbers[i] >= max){
+//             max2 = max;
+//             max = numbers[i];    
+//         }else{
+//             max2 = numbers[i];
+//         }
+//     }
+// }
