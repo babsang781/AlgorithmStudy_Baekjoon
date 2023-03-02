9f556ad4bb3fa06c39939513874040d87f841664
박상민 <89438832+babsang781@users.noreply.github.com>
2023-03-03 00:03:04 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.23 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int slice, int n) {
+        int answer = 1;
+        for(int i = 1; i <= n/2; i++){
+            if( answer * slice / n == 0){
+                answer++;
+            } else{
+                break; 
+            }
+        }
+        
+        return answer;
+    }
+}
