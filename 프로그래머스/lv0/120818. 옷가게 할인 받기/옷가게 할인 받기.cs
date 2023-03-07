2fec37914fd31af90de84a2a79bcbf6da6ac71a7
박상민 <89438832+babsang781@users.noreply.github.com>
2023-03-07 23:16:54 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        
+        if ( price >= 100000 && price < 300000) return price*95/100;
+        else if ( price >= 300000 && price < 500000) return price*9/10;
+        else if ( price >= 500000 ) return price*8/10;
+        else{
+            return price;
+                        
+        }        
+    }
+}
