8524e50a3d0bf16602f2863c6c5f42f79e2492cf
박상민 <89438832+babsang781@users.noreply.github.com>
2023-03-31 00:08:41 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.15 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.cs
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.cs
@@ -3,15 +3,8 @@ using System;
 public class Solution {
     public int[] solution(int n)
     {
-        int[] answer = new int[] { };
-
-        int num = 0;
-        if(n % 2 == 0) num = n/2; 
-        if(n % 2 == 1) num = n/2 + 1;
-
-        answer = new int[num];
-
-        for(int i = 0; i < num; i++)
+        int[] answer = new int[(n+1)/2] ;
+        for(int i = 0; i < (n+1)/2; i++)
         {
             answer[i] = i*2 + 1;
         }
