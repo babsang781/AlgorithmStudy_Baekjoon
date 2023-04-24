5359aebe79de65199b63046f077d24b85f33e586
박상민 <89438832+babsang781@users.noreply.github.com>
2023-04-24 21:04:57 +0900

    [unrated] Title: 홀짝 구분하기, Time: 25.64 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] s;
+
+        Console.Clear();
+        s = Console.ReadLine().Split(' ');
+
+        int a = Int32.Parse(s[0]);
+        
+        Console.Write(a + " is ");
+        Console.Write( a%2==0? "even": "odd");
+        
+    }
+}
