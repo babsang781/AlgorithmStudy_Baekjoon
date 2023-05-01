6b3e610090b0ee069b0920b29a5d4599f49fb45b
박상민 <89438832+babsang781@users.noreply.github.com>
2023-05-01 15:25:49 +0900

    [unrated] Title: 수 조작하기 1, Time: 0.62 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Solution {
+    public int solution(int n, string control) {
+        int answer = 0;
+        answer = n;
+        for( int i = 0 ; i < control.Length;i++){
+            switch(control[i]){
+                case 'w':
+                    answer++;
+                    break;
+                case 's':
+                    answer--;
+                    break;
+                case 'd':
+                    answer +=10;
+                    break;
+                case 'a':
+                    answer -=10;
+                    break;
+            }    
+        }
+        
+        return answer;
+    }
+}
