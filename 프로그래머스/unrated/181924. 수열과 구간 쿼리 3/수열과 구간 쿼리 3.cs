063dc17e2e2c2c25dd0e4c9b2fe0aea5b937ce60
박상민 <89438832+babsang781@users.noreply.github.com>
2023-05-01 16:38:31 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.22 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr, int[,] queries) {
+
+        int[] answer = arr;
+
+        for (int i = 0; i < queries.GetLength(0); i++)
+        {
+            int temp = answer[queries[i,0]];
+            answer[queries[i, 0]] = answer[queries[i, 1]];
+            answer[queries[i, 1]] = temp;
+        }
+
+        return answer;
+    }
+}
