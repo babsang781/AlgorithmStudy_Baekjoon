e029488fd845ca83b79200d08070e9708d9be224
박상민 <89438832+babsang781@users.noreply.github.com>
2023-05-05 08:53:58 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.18 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(string number) {
+        int sum = 0;
+        for(int i = 0 ; i < number.Length; i++){
+            sum += (int)(number[i])-48;
+        }
+        
+        return sum%9;
+    }
+}
