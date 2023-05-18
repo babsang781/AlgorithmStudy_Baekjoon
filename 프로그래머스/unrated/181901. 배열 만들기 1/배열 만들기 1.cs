631b4b02cf64a12d40b2f12caeedf32ad9f93321
박상민 <89438832+babsang781@users.noreply.github.com>
2023-05-18 22:37:31 +0900

    [unrated] Title: 배열 만들기 1, Time: 0.85 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int n, int k) {
+        List<int> list = new List<int>();
+        
+        for(int i = 1 ; i <= n ; i++){
+            if(i%k ==0) list.Add(i);
+            
+        }
+            
+        return list.ToArray();
+    }
+}
