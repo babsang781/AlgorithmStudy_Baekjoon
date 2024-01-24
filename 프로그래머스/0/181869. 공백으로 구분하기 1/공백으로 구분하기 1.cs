e54fa1dae8380362a3b28d4f8ce991de0fec6bc5
박상민 <89438832+babsang781@users.noreply.github.com>
2024-01-25 07:31:05 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.17 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public string[] solution(string my_string) {
+// 방법 : phrase.Split(' ');
+        
+        
+        string[] answer = my_string.Split(' ');
+        return answer;
+    }
+}
